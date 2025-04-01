6fc91536cb227aad3e39040f1cd9f9807f55ddc9
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-04-01 22:46:12 +0530

    Add files via upload

---
+++ _Q2_ReverseAnArray.java
@@ -0,0 +1,34 @@
+//import java.util.*;
+//
+//public class _Q2_ReverseAnArray
+//{
+//    public static void reverse(int arr)
+//    {
+//        int first = 0;
+//        int last = arr.length-1;
+//
+//        while(first < last)
+//        {
+//            //swap
+//            int temp = arr[last];
+//            arr[last] = arr[first];
+//            arr[first] = temp;
+//
+//            first ++;
+//            last--;
+//        }
+//    }
+//
+//    public static void main (String args[])
+//    {
+//        int arr[] = {2, 4, 6, 8, 10};
+//        reverse(arr);
+//
+//        //print
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            System.out.println(arr[i]+"");
+//        }
+//        System.out.println();
+//    }
+//}
