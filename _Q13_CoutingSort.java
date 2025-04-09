0b882a935c98256a79ac52e9719a6c6d8ff1e387
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-04-09 07:25:23 +0530

    Add files via upload

---
+++ _Q13_CoutingSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class _Q13_CoutingSort
+{
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for(int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        //sorting
+        int j = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = {5, 4, 1, 3, 2};
+        countingSort(arr);
+    }
+}
