bd1b03c508bebc6753ae8879363b413e1b86fda7
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-04-12 09:09:14 +0530

    Add files via upload

---
+++ _Q25_RecursionAscending.java
@@ -0,0 +1,18 @@
+public class _Q25_RecursionAscending
+{
+    // print number from 1 to n in recursion (Ascending order)
+
+        public static void printInc(int n){
+            if(n == 1){
+                System.out.println(n);
+                return;
+            }
+            printInc(n - 1);
+            System.out.println(n+" ");
+        }
+        public static void main(String[] args) {
+            int n = 10;
+            printInc(n);
+        }
+
+}
