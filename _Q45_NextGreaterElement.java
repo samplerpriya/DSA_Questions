12b5b1eb42b5e0d297b708720b28c51e820ac604
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-04-19 09:42:50 +0530

    Add files via upload

---
+++ _Q45_NextGreaterElement.java
@@ -0,0 +1,41 @@
+package _DSA_ApnaCollege;
+import java.util.*;
+public class _Q45_NextGreaterElement
+{
+    public static void main(String[] args) {
+        int arr[] = {6, 8, 0, 1, 3};
+        Stack<Integer> s = new Stack<>();
+        int nxtGreater[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // 1 while
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            // 2 if-else
+            if (s.isEmpty()) {
+                nxtGreater[i] = -1;
+
+            } else {
+                nxtGreater[i] = arr[s.peek()];
+
+            }
+
+            //3 push in stack
+            s.push(i);
+        }
+
+        for (int i = 0; i < nxtGreater.length; i++){
+            System.out.println(nxtGreater[i]+" ");
+        }
+        System.out.println();
+    }
+}
+
+
+// different type of question related to this
+// next Greater Right
+// next Greater Left
+// next smaler in the right hand side
+// next smaller left
